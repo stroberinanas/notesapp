6a7a81c0d446595430219f8022a3f6646dc6fc35
stroberinanas <sabilakhairina2@gmail.com>
2025-05-29 17:22:54 +0700

    upload18

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -603,7 +603,7 @@ export async function login(req, res) {
             secure: false // true jika HTTPS
         });
 
-        res.status(200).json({ accessToken });
+        res.status(200).json({ accessToken, refreshToken });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
