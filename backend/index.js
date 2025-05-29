6eaf98ad67aee0c88204bf8d924c9694e2447ec6
stroberinanas <sabilakhairina2@gmail.com>
2025-05-29 16:06:03 +0700

    upload13

--- backend/index.js
+++ backend/index.js
@@ -7,26 +7,12 @@ import path from "path";
 
 const app = express();
 
-const allowedOrigins = [
-  // "http://localhost:5500",
-  // "http://127.0.0.1:5500",
-  "https://notes-be-1013759214686.us-central1.run.app"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `Origin ${origin} tidak diizinkan oleh CORS`;
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: "https://h-08-451505.uc.r.appspot.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
