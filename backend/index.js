d3403e504d0e490662786c7268e0d8e0a8551cf9
stroberinanas <sabilakhairina2@gmail.com>
2025-05-29 16:57:46 +0700

    upload16

--- backend/index.js
+++ backend/index.js
@@ -13,16 +13,6 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://h-08-451505.uc.r.appspot.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // ← ini penting untuk preflight
-  }
-  next();
-});
 
 app.use(express.json());
 
--- frontend/utils.js
+++ frontend/utils.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://notes-be-1013759214686.us-central1.run.app";
+export const BASE_URL = "https://notes-notesbe085-1013759214686.us-central1.run.app";
